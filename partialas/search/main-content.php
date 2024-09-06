f79f2fe4559da043b8725ebb5484a0432314c5fe
sina malmir <sinamalmir2@gmail.com>
2024-09-06 13:56:37 +0330

    create search page and set custom style for search widget

--- partialas/search/main-content.php
+++ partialas/search/main-content.php
@@ -2,7 +2,8 @@
 <div class="container mb-10 mt-10">
 	<div>
 		<h2 class="text-center mb-10 font-bold text-3xl ">
-			<?php echo get_the_archive_title(); ?>
+			نتایج جستجو برای :
+			<?php the_search_query(); ?>
 		</h2>
 		<div class="grid grid-cols-1 md:grid-cols-12  gap-4">
 
@@ -11,36 +12,35 @@
 
 				<div  class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4  md:px-5  rounded-lg  ">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<a href="<?php the_permalink(); ?>" class="item-slider-custom  py-3 mb-5  px-4 border border-lightBorder
-					rounded-lg  ">
-						<?php
-						if (has_post_thumbnail()){
-							the_post_thumbnail('',array(
-								'class' => 'rounded-xl archive-img object-cover w-full ',
-								'alt'=> get_the_title(),
-							));
-						}else{
-							?>
-							<img class="max-w-[110px] rounded-lg object-cover" src="<?php echo
-								get_template_directory_uri().'/assets/img/test11.webp'
-							?>" alt="">
+						<a href="<?php the_permalink(); ?>" class="item-slider-custom  py-3 mb-5  px-4 border border-lightBorder rounded-lg  ">
 							<?php
-						}
-						?>
-						<h3 class="font-bold text-sm my-2  ">
-							<?php echo get_the_title(); ?>
-						</h3>
-                        <p class="font-normal text-xs text-lightMain mb-2">
-							<?php echo  get_the_excerpt(); ?>
-                        </p>
-						<span class="font-light text-lightMain text-xs flex items-center gap-2 ">
+							if (has_post_thumbnail()){
+								the_post_thumbnail('',array(
+									'class' => 'rounded-xl archive-img object-cover w-full ',
+									'alt'=> get_the_title(),
+								));
+							}else{
+								?>
+								<img class="max-w-[110px] rounded-lg object-cover" src="<?php echo
+									get_template_directory_uri().'/assets/img/test11.webp'
+								?>" alt="">
+								<?php
+							}
+							?>
+							<h3 class="font-bold text-sm my-2  ">
+								<?php echo get_the_title(); ?>
+							</h3>
+							<p class="font-normal text-xs text-lightMain mb-2">
+								<?php echo  get_the_excerpt(); ?>
+							</p>
+							<span class="font-light text-lightMain text-xs flex items-center gap-2 ">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-3">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                           </svg>
 
                           <?php echo get_the_date('j F Y'); ?>
                           </span>
-					</a>
+						</a>
 					<?php
 					endwhile;
 					endif;
